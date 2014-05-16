a53083e3d559af1087dc97c3ad1988ef8d47db82
SkyLight <shruykan-tmf@live.fr>
2014-05-16 11:55:48 +0200

    Modif  classe EcranExportation4

--- src/EcranExportation.java
+++ src/EcranExportation.java
@@ -1,12 +1,49 @@
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 public class EcranExportation extends JPanel {
 	JFrame fenetre;
+	JPanel panel1;
+	JPanel panel2;
 	
-	public EcranExportation(JFrame fenetre) {
+	public EcranExportation() {
 		super();
-		this.fenetre = fenetre;
+		JButton Button1 = new JButton("Retour");
+		
+		this.panel1 = new JPanel();
+		this.panel2 = new JPanel();
+		
+		JLabel test1 = new JLabel("test");
+		JLabel test2 = new JLabel("test2");
+		
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		panel1.setAlignmentX(CENTER_ALIGNMENT);
+		panel2.setAlignmentX(CENTER_ALIGNMENT);
+		
+		panel1.setLayout(new GridLayout(0,5));
+		panel2.setLayout(null);
+		
+		
+		panel1.add(test1);
+		panel1.add(test2);
+		panel2.add(Button1);
+		
+		this.add(panel1);
+		this.add(panel2);
+	}
+	
+	public static void main(String[] args) {
+		JFrame test = new JFrame();
+		test.getContentPane().add(new EcranExportation());
+		test.setSize(1366,  768);
+		test.setVisible(true);
+		
 	}
 }
