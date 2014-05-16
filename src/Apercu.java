329c401d14be7983c64b9e87279ac73499f44e82
SkyLight <shruykan-tmf@live.fr>
2014-05-16 11:06:14 +0200

    Modification Classe Apercu. Modification Classe Graphe : Ajout boucle pour afficher un nombres de rectangles correspond au nombre de couleurs voulus

--- src/Apercu.java
+++ src/Apercu.java
@@ -1,3 +1,4 @@
+import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.*;
@@ -7,8 +8,9 @@ public class Apercu extends JPanel {
 	
 	public Apercu(JFrame parent) {
 		super();
+		setPreferredSize(new Dimension(900,500));
 		Graphe graph = new Graphe();
-		this.apercu = new JLabel("Aperçu");
+		this.apercu = new JLabel("Apercu");
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
 		add(apercu);
--- src/Graphe.java
+++ src/Graphe.java
@@ -1,15 +1,30 @@
 import java.awt.*;
+
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 public class Graphe extends JPanel {
-	
+
 	public Graphe() {
-		
-		setSize(800,400);
+		super();
+		Dimension taille = new Dimension(800,200);
+		this.setMaximumSize(taille);
+		this.setPreferredSize(taille);
+		this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	}
-	
+
 	public void paintComponent(Graphics g) {
-		g.setColor(Color.BLUE);
-		g.fillRect(10, 800, 20, 20);
+		int x = 60;
+		int y = 200;
+		int hauteur = 20;
+		int largeur = 20;
+		int cpt = 0;
+		int nbCouleur = 8;
+
+		while(cpt < nbCouleur) {
+			g.setColor(Color.BLUE);
+			g.fillRect((x + (800/nbCouleur)*cpt) -largeur , y-hauteur, largeur , hauteur );
+			cpt++;
+		}
 	}
 }
