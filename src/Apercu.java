5126f4a41f77d112a29b978eccf041b658da46cb
SkyLight <shruykan-tmf@live.fr>
2014-05-16 10:00:02 +0200

    Modification Apercu

--- src/Apercu.java
+++ src/Apercu.java
@@ -1,6 +1,20 @@
-import javax.swing.JPanel;
+import java.awt.Graphics;
 
+import javax.swing.*;
 
 public class Apercu extends JPanel {
-
+	protected JLabel apercu;
+	private JFrame parent;
+	
+	public Apercu(JFrame parent) {
+		super();
+		this.parent = parent;
+		this.apercu = new JLabel("Aperçu");
+		this.setLayout(new BoxLayout(parent.getContentPane(), BoxLayout.Y_AXIS));
+	}
+	
+	public void paint() {
+		Graphics g = this.getGraphics();
+	}
+	
 }
