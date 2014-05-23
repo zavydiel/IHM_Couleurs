cea3c3a7498c994acf71ea9356350679289b2582
SkyLight <shruykan-tmf@live.fr>
2014-05-23 11:04:04 +0200

    Ecran Exportation Fonctionnel

--- src/exportation/EcranExportation.java
+++ src/exportation/EcranExportation.java
@@ -1,11 +1,15 @@
 package exportation;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import couleurs.GLCouleur;
 import couleurs.Palette;
 import fenetre.Fenetre;
 
@@ -14,36 +18,37 @@ public class EcranExportation extends JPanel {
 	JFrame fenetre;
 	JPanel panel1;
 	JPanel panel2;
-	
+
 	public EcranExportation(JFrame parent) {
 		super();
 		this.fenetre = parent;
-		JButton Button1 = new JButton("Retour");
-		
-		this.panel1 = new JPanel();
-		this.panel2 = new JPanel();
-		
-		JLabel test1 = new JLabel("test");
-		JLabel test2 = new JLabel("test2");
-		
+
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
-		panel1.setAlignmentX(CENTER_ALIGNMENT);
-		panel2.setAlignmentX(CENTER_ALIGNMENT);
-		
-		panel1.setLayout(new GridLayout(0,5));
-		panel2.setLayout(null);
-		
-		
-		panel1.add(test1);
-		panel1.add(test2);
-		panel2.add(Button1);
-		
-		this.add(panel1);
-		this.add(panel2);
+
+		for(int j=1;j<=getPalette().getTaille();j++) {
+			add(new PanelCouleur(getPalette().getGLCouleur(j-1),j));
+		}
+
+
+		JButton Retour = new JButton("Retour");
+
+		Retour.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				((Fenetre) fenetre).permuterEcran();
+
+			}
+		});
+
+		add(Retour);
 	}
-	
+
+
+
 	public Palette getPalette() {
 		return ((Fenetre) fenetre).getPalette();
 	}
+
+
 }
---
+++ src/exportation/PanelCouleur.java
@@ -0,0 +1,94 @@
+package exportation;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.TextField;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import couleurs.GLCouleur;
+
+public class PanelCouleur extends JPanel{
+	GLCouleur color;
+
+	public PanelCouleur(GLCouleur color, int i) {
+		this.color = color;
+		Box horizontalBox;
+		horizontalBox = Box.createHorizontalBox();
+		setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
+		setPreferredSize(new Dimension(1366,768));
+		setMaximumSize(new Dimension(1366,768));
+		
+		horizontalBox.add(Box.createGlue());
+		JLabel numCouleur = new JLabel("Couleur "+i);
+		horizontalBox.add(numCouleur);
+		horizontalBox.add(Box.createGlue());
+		
+		JLabel couleur= new JLabel(" ");
+		couleur.setPreferredSize(new Dimension(100,50));
+		couleur.setBackground(new Color(color.getRed(),color.getGreen(),color.getBlue()));
+		couleur.setOpaque(true);
+		horizontalBox.add(couleur);
+		horizontalBox.add(Box.createGlue());
+		
+		JLabel rouge = new JLabel("R : ");
+		TextField red = new TextField(""+color.getRed());
+		red.setPreferredSize(new Dimension(90,20));
+		red.setMaximumSize(new Dimension(90,20));
+		red.setEditable(false);
+		horizontalBox.add(rouge);
+		horizontalBox.add(red);
+		horizontalBox.add(Box.createGlue());
+		
+		JLabel vert = new JLabel("V : ");
+		TextField green = new TextField(""+color.getGreen());
+		green.setPreferredSize(new Dimension(90,20));
+		green.setMaximumSize(new Dimension(90,20));		
+		green.setEditable(false);
+		horizontalBox.add(vert);
+		horizontalBox.add(green);
+		horizontalBox.add(Box.createGlue());
+		  
+		JLabel bleu = new JLabel("B : ");
+		TextField blue = new TextField(""+color.getBlue());
+		blue.setPreferredSize(new Dimension(90,20));
+		blue.setMaximumSize(new Dimension(90,20));
+		blue.setEditable(false);
+		horizontalBox.add(bleu);
+		horizontalBox.add(blue);
+		horizontalBox.add(Box.createGlue());
+		
+		JLabel hexadecimal = new JLabel("Hex : ");
+		TextField Hexa = new TextField(String.format("#%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue()));
+		Hexa.setPreferredSize(new Dimension(90,20));
+		Hexa.setMaximumSize(new Dimension(90,20));		
+		Hexa.setEditable(false);
+		horizontalBox.add(hexadecimal);
+		horizontalBox.add(Hexa);
+		horizontalBox.add(Box.createGlue());
+		horizontalBox.add(Box.createGlue());
+		horizontalBox.add(Box.createGlue());
+
+		add(horizontalBox);
+		
+		
+		
+
+
+	}
+
+	/*public void paintComponent(Graphics g) {
+		g.setColor(new Color(color.getRed(),color.getGreen(),color.getBlue()));
+		g.fillRect(50,50,50,50);
+	}*/
+	
+	
+}
